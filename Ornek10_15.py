49134c9eac6730cd2b7b220a7ef8f8973c8a838a
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2022-11-10 15:19:49 +0300

    Update Ornek10_15.py

--- Ornek10_15.py
+++ Ornek10_15.py
@@ -12,9 +12,4 @@ print(list(karakter))
 Soru: Peki tersi işlemi nasıl  gerçekleştirebiliriz?
 Yani birden fazla karakter listesini tek bir string listesine nasıl dönüştürebiliriz?
 """
-k0_list = ['a', 'l', 'i']
-k1_list = ['b', 'a', 'd', 'e']
-k2_list = ['c', 'a', 'n']
-k3_list = ['b', 'e', 'r', 'a', 't']
-str_list = map(ambda x,y,z,t: x+y+z+t, k0_list,k1_list,k2_list,k3_ilst)
-print(list(str_list))
+
