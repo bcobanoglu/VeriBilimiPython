3da96c47feb2054271572201038cf1b5bc52e018
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2022-11-10 12:51:26 +0300

    Update Ornek10_15.py

--- Ornek10_15.py
+++ Ornek10_15.py
@@ -7,3 +7,13 @@ map() fonksiyonu ile kodlayalım.
 stringListe = ['ali', 'bade', 'can', 'berat']
 karakter = map(list, stringListe)
 print(list(karakter))
+
+"""
+Soru: Peki tersi işlemi nasıl  gerçekleştirebiliriz?
+Yani birden fazla karakter listesini tek bir string listesine nasıl dönüştürebiliriz?
+"""
+k0_list = ['a', 'l', 'i']
+k1_list = ['b', 'a', 'd', 'e']
+k2_list = ['c', 'a', 'n']
+k3_list = ['b', 'e', 'r', 'a', 't']
+str_list = map(ambda x,y,z,t: x+y+z+t, k0_list,k1_list,k2_list,k3_ilst)
